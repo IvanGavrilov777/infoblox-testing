ca04dfddf52c48c6c7d382238ce39997edc938f4
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-04-19 00:07:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "infoblox" {
 
 resource "infoblox_a_record" "a_rec_1_imported" {
     fqdn = "rec-a-1.imported.test.com"
-    ip_addr = "192.168.1.2"
+    ip_addr = "78.83.117.100"
     ttl = 10
     comment = "A-record to be imported"
     ext_attrs = jsonencode({
